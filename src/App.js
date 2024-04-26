c06fcd1a93c405ee3bc9599a158e46d1474d6609
“satyam141026cs” <satyamrsharma141026@gmail.com>
2024-04-26 15:42:50 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ChartComponent from './ChartComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ChartComponent/>
+    </div>
+  );
+}
+
+export default App;
